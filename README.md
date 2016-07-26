e86379911f777c12b7b7d43d138ba2879582513b
Will Xu <williamxu15@gmail.com>
2016-07-25 20:56:19 -0400

    spiral walk implemented

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Uses pokemon go node api
 
 TODO:<br/>
 [X] find pokemon nearby<br/>
+[X] walk implemented to find gps location of nearby pokemon<br/>
 [] check to see if nearby pokemon or catchable pokemon provides anything useful<br/>
 [] implement slackbot to constantly check area for pokemon
--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var PokemonGO = require('pokemon-go-node-api');
 var a = new PokemonGO.Pokeio();
 
 var fs = require('fs');
-
+var util = require('./util');
 var config = fs.readFileSync('config.json');
 config = JSON.parse(config);
 
@@ -23,30 +23,66 @@ var username = config.username;
 var password = config.password;
 var provider = config.auth;
 
+var hb = function(loc) {
+    a.SetLocation({type:'coords', coords:{latitude: loc.lat, longitude: loc.lng, altitude:0}}, function(err, cord){
+        a.Heartbeat(function(err, hb){
+            if(err){
+                console.log(err);
+            }
+            for (var i = hb.cells.length -1; i>=0; i--) {
+                if (hb.cells[i].WildPokemon[0]) {
+                    for (var j = 0; j < hb.cells[i].WildPokemon.length; j++) {
+                        var pokemon = a.pokemonlist[parseInt(hb.cells[i].WildPokemon[j].pokemon.PokemonId)];
+                        console.log(pokemon.name +' at '+hb.cells[i].WildPokemon[j].Latitude+', '+hb.cells[i].WildPokemon[j].Longitude);
+                        var time = new Date((new Date()).getTime() + hb.cells[i].WildPokemon[j].TimeTillHiddenMs);
+                        console.log('Hides at: '+ time.toLocaleTimeString());
+                    }
+                }
+            }
+        });
+    });
+}
+
 a.init(username, password, location, provider, function(err) {
     if (err) throw err;
 
     console.log('1[i] Current location: ' + a.playerInfo.locationName);
     console.log('1[i] lat/long/alt: : ' + a.playerInfo.latitude + ' ' + a.playerInfo.longitude + ' ' + a.playerInfo.altitude);
 
-//    var origin = new s2.S2CellId(new s2.S2LatLng(a.playerInfo.latitude, a.playerInfo.longitude)).parent(15);
-//    console.log(origin.id());
+    var spiral = util.generate_spiral(a.playerInfo.latitude, a.playerInfo.longitude, 0.0015, 16);
+
+    console.log(spiral);
+    var index =0;
+
+    setInterval(function() {
+        if (index < spiral.length) {
+            hb(spiral[index]);
+            index++;
+        } else {
+            process.exit();
+        }
+    }, 2000);
     
-    a.Heartbeat(function(err, hb){
-        if(err){
-            console.log(err);
-        }
-        for (var i = hb.cells.length -1; i>=0; i--) {
-            if (hb.cells[i].WildPokemon[0]) {
-                for (var j = 0; j < hb.cells[i].WildPokemon.length; j++) {
-                    var pokemon = a.pokemonlist[parseInt(hb.cells[i].WildPokemon[j].pokemon.PokemonId)];
-                    console.log(pokemon.name +' at '+hb.cells[i].WildPokemon[j].Latitude+', '+hb.cells[i].WildPokemon[j].Longitude);
-                    var time = new Date((new Date()).getTime() + hb.cells[i].WildPokemon[j].TimeTillHiddenMs);
-                    console.log('Hides at: '+ time.toLocaleTimeString());
-                }
-            } else {
-                console.log('No pokemon in cell:', hb.cells[i].S2CellId.toString());
-            }
-        }
-    });
+
+
+    // a.Heartbeat(function(err, hb){
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     for (var i = hb.cells.length -1; i>=0; i--) {
+    //         if (hb.cells[i].WildPokemon[0]) {
+    //             for (var j = 0; j < hb.cells[i].WildPokemon.length; j++) {
+    //                 var pokemon = a.pokemonlist[parseInt(hb.cells[i].WildPokemon[j].pokemon.PokemonId)];
+    //                 console.log(pokemon.name +' at '+hb.cells[i].WildPokemon[j].Latitude+', '+hb.cells[i].WildPokemon[j].Longitude);
+    //                 var time = new Date((new Date()).getTime() + hb.cells[i].WildPokemon[j].TimeTillHiddenMs);
+    //                 console.log('Hides at: '+ time.toLocaleTimeString());
+    //             }
+    //         }
+    //     }
+    // });
 });
+
+
+
+
+
